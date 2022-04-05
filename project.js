d1502de5dba7a17a8dcd2ce20f443e8a75855d28
elmore <eldad.ab@walla.co.il>
2022-04-05 19:34:59 +0300

    cards random

--- project.js
+++ project.js
@@ -1,7 +1,23 @@
-const cards = ["A","B","C","A","B","C"];
-for(i of cards){
-    let divs  = document.createElement("div");
-    divs.innerHTML = i
-    const board = document.getElementById("board")
-    board.appendChild(divs);
+const cards = ["A","B","C","D","F","A","B","C","D","F"];
+shufel(cards);
+const board = document.getElementById("board")
+for(i in cards){
+    const element = createCard(i)
+    board.appendChild(element);
+}
+
+function shufel(cards) {
+    cards = cards.sort(() => Math.random() - 0.5)}
+//     for(i in cards){
+//         let card = cards[i]
+//         cards.splice(i,1)
+//         cards.splice(Math.floor(Math.random() * (i + 1)),0,card)
+//     }
+// }
+function createCard(idx){
+    const cardEL = document.createElement("div");
+    cardEL.innerHTML = cards[idx];
+    cardEL.id = idx
+    cardEL.className = "card"
+    return cardEL
 }
