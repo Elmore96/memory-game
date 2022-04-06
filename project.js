0a6837d4a5fa654831bff8e912967ce528ebf061
elmore <eldad.ab@walla.co.il>
2022-04-06 17:34:07 +0300

    set amount of random cards

--- project.js
+++ project.js
@@ -1,19 +1,30 @@
-const cards = ["A","B","C","D","F","A","B","C","D","F"];
+const cardsOptions = ["A","B","C","D","E","F","G","H","I","J"];
+let numberOfcard = Number(prompt("how many diffrent cards types?\n*btween 2 to 10"));
+let cards = []
+numberOfcards()
+
 shufel(cards);
 const board = document.getElementById("board")
 for(i in cards){
     const element = createCard(i)
     board.appendChild(element);
 }
+function numberOfcards() {
+    while(isNaN(numberOfcard) || numberOfcard <2 || numberOfcard >10){
+        numberOfcard = Number(prompt("enter valid number"))
+    }
+    let temp = cardsOptions.slice(0,(numberOfcard))
+    return cards = temp.concat(temp)
+}
 
-function shufel(cards) {
-    cards = cards.sort(() => Math.random() - 0.5)}
-//     for(i in cards){
-//         let card = cards[i]
-//         cards.splice(i,1)
-//         cards.splice(Math.floor(Math.random() * (i + 1)),0,card)
-//     }
-// }
+function shufel(arr) {
+    // cards = cards.sort(() => Math.random() - 0.5)}
+    for(i in arr){
+        let card = arr[i]
+        arr.splice(i,1)
+        arr.splice(Math.floor(Math.random() * cards.length),0,card)
+    }
+}
 function createCard(idx){
     const cardEL = document.createElement("div");
     cardEL.innerHTML = cards[idx];
