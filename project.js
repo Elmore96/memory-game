8684d52cb97080ab168a1eea80ff0f80742bfe67
elmore <eldad.ab@walla.co.il>
2022-04-22 09:55:49 +0300

    working flip

--- project.js
+++ project.js
@@ -1,16 +1,24 @@
+let numberOfcard;
+    openCards = [];
+    sameCards = 0;
+    cards = []
 const cardsOptions = ["A","B","C","D","E","F","G","H","I","J"];
-let numberOfcard = Number(prompt("how many diffrent cards types?\n*btween 2 to 10"));
-let cards = cardsOptions
+gameStart()
+function gameStart(){
+    numberOfcard = Number(prompt("how many diffrent cards types?\n*btween 2 to 10"));
 numberOfcards()
 
 shufel(cards);
-const board = document.getElementById("board")
+const board = document.getElementById("board");
+    while(board.firstChild){
+    board.removeChild(board.firstChild);
+}
 for(i in cards){
     const element = createCard(i)
     board.appendChild(element);
 }
-
-
+const allCards = document.getElementsByClassName('card')
+}
 function numberOfcards() {
     while(isNaN(numberOfcard) || numberOfcard <2 || numberOfcard >10){
         numberOfcard = Number(prompt("enter valid number"))
@@ -20,7 +28,6 @@ function numberOfcards() {
 }
 
 function shufel(arr) {
-    // cards = cards.sort(() => Math.random() - 0.5)}
     for(i in arr){
         let card = arr[i]
         arr.splice(i,1)
@@ -31,6 +38,30 @@ function createCard(idx){
     const cardEL = document.createElement("div");
     cardEL.innerHTML = cards[idx];
     cardEL.id = idx
-    cardEL.className = "card" + " " + cardEL.innerHTML
+    cardEL.className = cardEL.innerHTML + " card " + "back" 
     return cardEL
 }
+board.addEventListener('click', () =>{
+    if(openCards.length == 2){
+        for(i of openCards){
+            i.className = i.innerHTML + " card " + "back" 
+        }
+        openCards = []
+    }
+})
+
+board.addEventListener('click',(i)=>{
+    if(i.target == document.getElementById('board') || i.target == openCards[0] || i.target.className.includes('flip')){
+    }else{
+    let target = i.target;
+    target.className = "card flip " + target.innerHTML;
+    openCards.push(target)
+}
+
+    if(openCards.length == 2){
+        if(openCards[0].innerHTML == openCards[1].innerHTML){
+            sameCards += 2
+            openCards = []
+        }
+    }
+})
